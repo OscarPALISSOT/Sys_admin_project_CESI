d979cb1f41cb73583ad623d8947dfcc0b77a556b
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-10-20 15:45:02 +0200

    update test root

--- setupRoot.sh
+++ setupRoot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # test si root
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+if ["$(id -u)" != "0"]; then
+   echo "This script must be run as root" 1>&2
    exit 1
 fi
 
