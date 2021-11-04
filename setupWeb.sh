0e92573afe036264a509e774eb173f245983cf1b
KNFreed <44784500+KNFreed@users.noreply.github.com>
2021-11-04 19:04:52 +0100

    Update setupWeb.sh

--- setupWeb.sh
+++ setupWeb.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# test si root
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
 # Création du dossier répertoire de nos Docker
-
 mkdir -p ~/.docker
-chmod 777 ~/.docker
 
 # Copie du Site Web
 git -C ~/.docker clone https://github.com/KNFreed/PW-Docker-Web.git
