2c8dc59d673e5a82b541f03917876b01053700e6
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2021-11-05 12:22:36 +0100

    Update setupCentreon.sh

--- setupCentreon.sh
+++ setupCentreon.sh
@@ -15,7 +15,7 @@ chmod 777 ~/.docker
 # Copie du service Centreon
 git -C ~/.docker clone --recurse-submodules https://github.com/EarthLab-Luxembourg/docker-centreon.git
 wget https://raw.githubusercontent.com/OscarPALISSOT/projet-SI/main/VMware-vSphere-Perl-SDK-7.0.0-16453907.x86_64.tar.gz -P ~/.docker/docker-centreon/buildenv/files/other/vmware
-sleep 5
+sed -i 's/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz/VMware-vSphere-Perl-SDK-7.0.0-16453907.x86_64.tar.gz/g' ~/.docker/docker-centreon/CONFIG
 # Execution du script de build
 (cd ~/.docker/docker-centreon && ./build.sh)
 
