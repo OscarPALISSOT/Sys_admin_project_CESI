812f7f3fb6c48b63fda070f99bb66864f6385570
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-05 17:05:40 +0100

    update

--- setupDomainMaster.ps1
+++ setupDomainMaster.ps1
@@ -1,27 +1,8 @@
 <# creation du domain #>
 
-<# ajout de (AD-Domain-Services), le DNS (DNS) et les outils d'administration graphique (RSAT-AD-Tools) #>
+<# ajout de (AD-Domain-Services) #>
 
-$FeatureList = @("RSAT-AD-Tools","AD-Domain-Services","DNS")
-
-Foreach($Feature in $FeatureList){
-
-   if(((Get-WindowsFeature-Name $Feature).InstallState)-eq"Available"){
-
-     Write-Output"Feature $Feature will be installed now !"
-
-     Try{
-
-        Add-WindowsFeature-Name $Feature-IncludeManagementTools -IncludeAllSubFeature
-
-        Write-Output"$Feature : Installation is a success !"
-
-     }Catch{
-
-        Write-Output"$Feature : Error during installation !"
-     }
-   } # if(((Get-WindowsFeature -Name $Feature).InstallState) -eq "Available")
-} # Foreach($Feature in $FeatureList)
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 
 <# creation domain #>
 
