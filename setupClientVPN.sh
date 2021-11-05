a04a844cc0040da4240b30d275ccb7992b45036e
KNFreed <44784500+KNFreed@users.noreply.github.com>
2021-11-05 03:35:26 +0100

    Update setupClientVPN.sh

---
+++ setupClientVPN.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# test si root ou sudo
+if [[ $EUID -eq 0 ]]; then
+    echo "Lancement de l'installation du serveur Web"
+else
+    echo "Merci de lancer en root ou sudo."
+    exit 1
+fi
+
+read -p "Nombre d'utilisateurs à créer : " numberuser
+
+for ((i = 1; i <= numberuser; i++))
+do
+  docker run -v /etc/openvpn-conf:/etc/openvpn --rm -it docker-openvpn easyrsa build-client-full "user$i" nopass
+  docker run -v /etc/openvpn-conf:/etc/openvpn --rm docker-openvpn ovpn_getclient "user$i" > "user$i.ovpn"
+done
+
