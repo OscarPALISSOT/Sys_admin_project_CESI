bcc4e0da50f29c7aa850fc1d316a911ada4f38fb
Frédéric Spataro <44784500+KNFreed@users.noreply.github.com>
2021-11-05 11:23:01 +0100

    Create setupCentreon.sh

---
+++ setupCentreon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# test si root ou sudo
+if [[ $EUID -eq 0 ]]; then
+    echo "Lancement de l'installation du serveur VPN"
+else
+    echo "Merci de lancer en root ou sudo."
+    exit 1
+fi
+
+# Création du dossier répertoire de nos Docker
+mkdir -p ~/.docker
+chmod 777 ~/.docker
+
+# Copie du service Centreon
+git -C ~/.docker clone https://github.com/EarthLab-Luxembourg/docker-centreon.git
+
+# Execution du script de build
+(cd ~/.docker ; sh build.sh)
+
+# Execution du script de run
+(cd ~/.docker ; sh run.sh)
