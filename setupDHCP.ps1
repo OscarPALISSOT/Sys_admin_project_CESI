0133a986179b5c4a8e71a9b6f0e7e5636c05636a
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-05 17:25:14 +0100

    update

--- setupDHCP.ps1
+++ setupDHCP.ps1
@@ -15,8 +15,8 @@ Add-DhcpServerInDC -DnsName Master.abstergo.local -IPAddress 192.168.106.3
 Add-DhcpServerV4Scope -Name "Lan" -StartRange 192.168.106.100 -EndRange 192.168.106.250 -SubnetMask 255.255.255.0
 
 <# gateway et dns #>
-Set-DhcpServerV4OptionValue -DnsServer 192.168.1.3 -Router 192.168.1.2
-Set-DhcpServerv4Scope -ScopeId 192.168.1.3 -LeaseDuration 8.00:00:00
+Set-DhcpServerV4OptionValue -DnsServer 192.168.106.3 -Router 192.168.106.2
+Set-DhcpServerv4Scope -ScopeId 192.168.106.3 -LeaseDuration 8.00:00:00
 Set-DhcpServerv4OptionDefinition -OptionId 15 -DefaultValue abstergo.local
 
 
