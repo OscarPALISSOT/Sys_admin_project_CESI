08dd1d50d9030fb7cadff94a6ca48b596d094a1d
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-05 15:54:25 +0100

    update

---
+++ setupDHCP.ps1
@@ -0,0 +1,23 @@
+<# mise en place du DHCP #>
+
+<# installation role #>
+Install-WindowsFeature -Name DHCP -IncludeManagementTools
+
+<# ajout du groupe secu DHCP #>
+Add-DhcpServerSecurityGroup
+
+Restart-Service dhcpserver
+
+<# ajout dans le domaine #>
+Add-DhcpServerInDC -DnsName Master.abstergo.local -IPAddress 192.168.106.3
+
+<# création de la pool lan #>
+Add-DhcpServerV4Scope -Name "Lan" -StartRange 192.168.106.100 -EndRange 192.168.106.250 -SubnetMask 255.255.255.0
+
+<# gateway et dns #>
+Set-DhcpServerV4OptionValue -DnsServer 192.168.1.3 -Router 192.168.1.2
+Set-DhcpServerv4Scope -ScopeId 192.168.1.3 -LeaseDuration 8.00:00:00
+Set-DhcpServerv4OptionDefinition -OptionId 15 -DefaultValue abstergo.local
+
+
+Restart-service dhcpserver
--- setupMasterServer.ps1
+++ setupMasterServer.ps1
@@ -4,7 +4,7 @@
 Rename-Computer -NewName Master -Force
 
 <# changement de l'ip  #>
-New-NetIPAddress -IPAddress "192.168.106.1" -PrefixLength "24" -InterfaceIndex (Get-NetAdapter).ifIndex -DefaultGateway "192.168.106.254"
+New-NetIPAddress -IPAddress "192.168.106.3" -PrefixLength "24" -InterfaceIndex (Get-NetAdapter).ifIndex -DefaultGateway "192.168.106.2"
 
 <# changement du dns #>
 Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -ServerAddresses ("127.0.0.1")
