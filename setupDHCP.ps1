5ed401c12d563a35f1c3618e365251b772c5300d
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-06 23:02:32 +0100

    update router

--- setupDHCP.ps1
+++ setupDHCP.ps1
@@ -12,7 +12,7 @@ Restart-Service dhcpserver
 Add-DhcpServerV4Scope -Name "Lan" -StartRange 192.168.106.100 -EndRange 192.168.106.250 -SubnetMask 255.255.255.0 -State Active
 
 <# gateway et dns #>
-Set-DhcpServerV4OptionValue -DnsServer 192.168.106.3 -Router 192.168.106.2
+Set-DhcpServerV4OptionValue -DnsServer 192.168.106.3 -Router 192.168.106.99
 Set-DhcpServerv4Scope -ScopeId 192.168.106.3 -LeaseDuration 8.00:00:00
 
 Restart-service dhcpserver
