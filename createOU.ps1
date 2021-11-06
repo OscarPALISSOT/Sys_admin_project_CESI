6663f5b20df13a5f26012a79e112a7bb212b748a
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-06 23:00:46 +0100

    createOU

---
+++ createOU.ps1
@@ -0,0 +1,18 @@
+New-ADOrganizationalUnit -Name "Direction"
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+
+New-ADOrganizationalUnit -Name "Compta"
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+
+New-ADOrganizationalUnit -Name "Markating"
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+
+New-ADOrganizationalUnit -Name "Com"
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+
+New-ADOrganizationalUnit -Name "R&D"
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
+New-ADUser -Name "Pdg 1" -GivenName "Pdg" -Surname "1" -SamAccountName "Pdg1" -UserPrincipalName "Pdg1@abstergo.fr" -Path "OU=Direction,DC=abstergo,DC=local" -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -force) -Enabled $true -ChangePasswordAtLogon $true
