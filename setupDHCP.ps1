c68baf9c6e20ea1de9679d77b1ffdae8ecbc26c1
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-06 16:36:42 +0100

    update

--- setupDHCP.ps1
+++ setupDHCP.ps1
@@ -8,15 +8,20 @@ Add-DhcpServerSecurityGroup
 
 Restart-Service dhcpserver
 
-<# ajout dans le domaine #>
-Add-DhcpServerInDC -DnsName Master.abstergo.local -IPAddress 192.168.106.3
-
 <# création de la pool lan #>
-Add-DhcpServerV4Scope -Name "Lan" -StartRange 192.168.106.100 -EndRange 192.168.106.250 -SubnetMask 255.255.255.0
+Add-DhcpServerV4Scope -Name "Lan" -StartRange 192.168.106.100 -EndRange 192.168.106.250 -SubnetMask 255.255.255.0 -State Active
 
 <# gateway et dns #>
 Set-DhcpServerV4OptionValue -DnsServer 192.168.106.3 -Router 192.168.106.2
 Set-DhcpServerv4Scope -ScopeId 192.168.106.3 -LeaseDuration 8.00:00:00
 
 Restart-service dhcpserver
- 
+
+<# ajout dans le domaine #>
+Add-DhcpServerInDC -DnsName Master.abstergo.local -IPAddress 192.168.106.3
+
+Restart-service dhcpserver
+
+Set-ItemProperty –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 –Name ConfigurationState –Value 2
+
+Restart-service dhcpserver
