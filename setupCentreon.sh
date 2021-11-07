c3fd3394252b2b169417f3db089a8c21740e26ba
KNFreed <44784500+KNFreed@users.noreply.github.com>
2021-11-07 15:44:12 +0100

    Update setupCentreon.sh

--- setupCentreon.sh
+++ setupCentreon.sh
@@ -13,7 +13,7 @@ mkdir -p ~/.docker
 chmod 777 ~/.docker
 
 # Installation container db
-docker run -itd --net=bridge -e MYSQL_ROOT_PASSWORD=secret --name centreon-db mariadb
+docker run -d --net=bridge -e MYSQL_ROOT_PASSWORD=secret --name centreon-db mariadb
 # modif cnf
 docker exec -i centreon-db sed -i 's/\[client-server]/[client-server]\nbind-address 0.0.0.0/' /etc/mysql/my.cnf
 # ajout compte
