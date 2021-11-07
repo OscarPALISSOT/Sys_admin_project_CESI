275b472e782f15de82a16f575e79f8c7e47774b3
KNFreed <44784500+KNFreed@users.noreply.github.com>
2021-11-07 15:30:06 +0100

    Update setupCentreon.sh

--- setupCentreon.sh
+++ setupCentreon.sh
@@ -12,16 +12,6 @@ fi
 mkdir -p ~/.docker
 chmod 777 ~/.docker
 
-# Copie du service Centreon
-git -C ~/.docker clone --recurse-submodules https://github.com/KNFreed/docker-centreon.git
-wget https://raw.githubusercontent.com/OscarPALISSOT/projet-SI/main/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz -P ~/.docker/docker-centreon/buildenv/files/other/vmware
-#sed -i 's/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz/VMware-vSphere-Perl-SDK-7.0.0-16453907.x86_64.tar.gz/g' ~/.docker/docker-centreon/CONFIG
-# Execution du script de build
-(cd ~/.docker/docker-centreon && ./build.sh)
-
-# Execution du script de run
-(cd ~/.docker/docker-centreon && ./run.sh)
-docker exec -i centreon service apache2 restart
 # Installation container db
 docker run -itd --net=bridge -e MYSQL_ROOT_PASSWORD=secret  --name centreon-db  mariadb
 # modif cnf
@@ -33,4 +23,15 @@ CREATE USER 'admin'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 EOF
+
+# Copie du service Centreon
+git -C ~/.docker clone --recurse-submodules https://github.com/KNFreed/docker-centreon.git
+wget https://raw.githubusercontent.com/OscarPALISSOT/projet-SI/main/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz -P ~/.docker/docker-centreon/buildenv/files/other/vmware
+#sed -i 's/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz/VMware-vSphere-Perl-SDK-7.0.0-16453907.x86_64.tar.gz/g' ~/.docker/docker-centreon/CONFIG
+# Execution du script de build
+(cd ~/.docker/docker-centreon && ./build.sh)
+
+# Execution du script de run
+(cd ~/.docker/docker-centreon && ./run.sh)
+
 # subnetwork
