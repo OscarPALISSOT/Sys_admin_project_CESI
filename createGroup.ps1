f27c707da3d5da072c66af1698ccb8cc1bd61a83
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-07 18:59:12 +0100

    updateeeeeeeee

--- createGroup.ps1
+++ createGroup.ps1
@@ -1,17 +1,17 @@
 New-ADGroup "communication" -Path "OU=communication,DC=abstergo,dc=local" -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members Com1, Com2
+Add-AdGroupMember -Identity communication -Members Com1, Com2
 
 New-ADGroup "compta" -Path "OU=compta,DC=abstergo,dc=local" -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members Compta1, Compta2
+Add-AdGroupMember -Identity compta -Members Compta1, Compta2
 
 New-ADGroup "direction" -Path "OU=direction,DC=abstergo,dc=local" -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members Pdg1
+Add-AdGroupMember -Identity direction -Members Pdg1
 
 New-ADGroup "marketing" -Path "OU=marketing,DC=abstergo,dc=local" -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members Marketing1, Marketing2
+Add-AdGroupMember -Identity marketing -Members Marketing1, Marketing2
 
 New-ADGroup "recherche" -Path "OU=recherche,DC=abstergo,dc=local" -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members 'R&D1', 'R&D2'
+Add-AdGroupMember -Identity recherche -Members 'R&D1', 'R&D2'
 
 New-ADGroup "abstergo"  -GroupScope Global
-Add-AdGroupMember -Identity Quality -Members Com1, Com2, Compta1, Compta2, Pdg1, Marketing1, Marketing2, 'R&D1', 'R&D2'
+Add-AdGroupMember -Identity abstergo -Members Com1, Com2, Compta1, Compta2, Pdg1, Marketing1, Marketing2, 'R&D1', 'R&D2'
