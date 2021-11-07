1daefc1ff56e9dc60dc21b0347564360b71ea77a
KNFreed <44784500+KNFreed@users.noreply.github.com>
2021-11-07 12:49:18 +0100

    Update setupCentreon.sh

--- setupCentreon.sh
+++ setupCentreon.sh
@@ -25,7 +25,11 @@ sed -i 's/VMware-vSphere-Perl-SDK-6.5.0-4566394.x86_64.tar.gz/VMware-vSphere-Per
 # Installation container db
 docker run -itd  -e MYSQL_ROOT_PASSWORD=secret  --name centreon-db  mariadb
 # ajout compte
-
+docker exec -i centreon-db mysql -u root -psecret mysql <<EOF
+CREATE USER 'admin'@'%' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
 # modif cnf
-
+sed -i 'S/\[server-client]/[server-client]\nbind-address O.O.O.O' /etc/mysql/my.cnf
 # subnetwork
