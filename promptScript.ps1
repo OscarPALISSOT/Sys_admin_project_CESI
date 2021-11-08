fec008342d7caec964bae99661909df9f8554a13
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-08 23:26:30 +0100

    test

--- promptScript.ps1
+++ promptScript.ps1
@@ -22,7 +22,7 @@ function CreateUser {
      -Surname $NewUser.SurName `
      -SamAccountName $NewUser.SamAccountName `
      -UserPrincipalName $NewUser.UserPrincipalName `
-     -Path "DC=" + $NewUser.OU + ",DC=abstergo,DC=local" `
+     -Path "OU=" + $NewUser.OU + ",DC=abstergo,DC=local" `
      -AccountPassword(Read-Host -AsSecureString "Entrez le mot de passe") `
      -Enabled $true `
      -ChangePasswordAtLogon $true
