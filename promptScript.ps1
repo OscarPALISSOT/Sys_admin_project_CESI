e4332dbc27916cfb4fb4a8ac37a8c66d9d081e06
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-08 23:01:13 +0100

    test

--- promptScript.ps1
+++ promptScript.ps1
@@ -1,28 +1,3 @@
-
-Write-Output "Que voulez vous faire ?"
-Write-Output "1 : Creer un utilisateur"
-Write-Output "2 : Creer une unite d'organisation"
-Write-Output "3 : Creer un groupe"
-Write-Output "4 : ajouter un utilisateur dans un groupe"
-Write-Output "5 : Supprimer un utilisateur"
-Write-Output "6 : Supprimer une unité d'organisation"
-Write-Output "7 : Supprimer un groupe"
-Write-Output "8 : Supprimer un utilisateur dans un groupe"
-
-$choice = Read-Host "Votre choix : "
-
-switch ( $choice )
-{
-    1 {   Write-Output "test 1"  }
-    2 {   Write-Output "test2" }
-    3 {  Write-Output "test"}
-    4 { Write-Output "test"  }
-    5 {   Write-Output "test"  }
-    6 {  Write-Output "test" }
-    7 {  Write-Output "test" }
-    8 { Write-Output "test" }
-}
-
 class User {
     [string]$GivenName
     [string]$SurName
@@ -35,14 +10,14 @@ class User {
 
 function CreateUser {
     $NewUser = [User]::new()
-    $NewUser.GivenName = Read-Host "Entrez le prénom : "
-    $NewUser.SurName = Read-Host "Entrez le nom : "
-    $NewUser.AccountPassword = Read-Host "Entrez le mot de passe : "
+    $NewUser.GivenName = Read-Host "Entrez le prénom "
+    $NewUser.SurName = Read-Host "Entrez le nom "
+    $NewUser.AccountPassword = Read-Host "Entrez le mot de passe "
     $NewUser.Name = $NewUser.GivenName + " " + $NewUser.SurName
     $NewUser.SamAccountName = $NewUser.GivenName + $NewUser.SurName
     $NewUser.UserPrincipalName = $NewUser.SamAccountName + "@abstergo.local"
     Get-ADOrganizationalUnit -Filter * | Format-Table Name
-    $NewUser.OU = Read-Host "Entrez une OU : "
+    $NewUser.OU = Read-Host "Entrez une OU "
     New-ADUser `
      -Name $NewUser.Name `
      -GivenName $NewUser.GivenName `
@@ -54,5 +29,31 @@ function CreateUser {
      -Enabled $true `
      -ChangePasswordAtLogon $true
 }
+
+
+Write-Output "Que voulez vous faire ?"
+Write-Output "1 : Creer un utilisateur"
+Write-Output "2 : Creer une unite d'organisation"
+Write-Output "3 : Creer un groupe"
+Write-Output "4 : ajouter un utilisateur dans un groupe"
+Write-Output "5 : Supprimer un utilisateur"
+Write-Output "6 : Supprimer une unité d'organisation"
+Write-Output "7 : Supprimer un groupe"
+Write-Output "8 : Supprimer un utilisateur dans un groupe"
+
+$choice = Read-Host "Votre choix "
+
+switch ( $choice )
+{
+    1 {   Get-CreateUser  }
+    2 {   Write-Output "test2" }
+    3 {  Write-Output "test"}
+    4 { Write-Output "test"  }
+    5 {   Write-Output "test"  }
+    6 {  Write-Output "test" }
+    7 {  Write-Output "test" }
+    8 { Write-Output "test" }
+}
+
 
 
