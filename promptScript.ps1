9b74c36ac7ad846013200d4ee2feb835635b8bc6
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-08 22:47:51 +0100

    create prompt !!!!!!

---
+++ promptScript.ps1
@@ -0,0 +1,46 @@
+
+Write-Output "Que voulez vous faire ?"
+Write-Output "1 : Créer un utilisateur"
+Write-Output "2 : Créer une unité d'organisation"
+Write-Output "3 : Créer un groupe"
+Write-Output "4 : ajouter un utilisateur dans un groupe"
+Write-Output "5 : Supprimer un utilisateur"
+Write-Output "6 : Supprimer une unité d'organisation"
+Write-Output "7 : Supprimer un groupe"
+Write-Output "8 : Supprimer un utilisateur dans un groupe"
+
+<# $choice = Read-Host "Votre choix : " #>
+
+class User {
+    [string]$GivenName
+    [string]$SurName
+    [string]$AccountPassword
+    [string]$Name
+    [string]$SamAccountName
+    [string]$UserPrincipalName
+    [string]$OU
+}
+
+function CreateUser {
+    $NewUser = [User]::new()
+    $NewUser.GivenName = Read-Host "Entrez le prénom : "
+    $NewUser.SurName = Read-Host "Entrez le nom : "
+    $NewUser.AccountPassword = Read-Host "Entrez le mot de passe : "
+    $NewUser.Name = $NewUser.GivenName + " " + $NewUser.SurName
+    $NewUser.SamAccountName = $NewUser.GivenName + $NewUser.SurName
+    $NewUser.UserPrincipalName = $NewUser.SamAccountName + "@abstergo.local"
+    Get-ADOrganizationalUnit -Filter * | Format-Table Name
+    $NewUser.OU = Read-Host "Entrez une OU : "
+    New-ADUser `
+     -Name $NewUser.Name `
+     -GivenName $NewUser.GivenName `
+     -Surname $NewUser.SurName `
+     -SamAccountName $NewUser.SamAccountName `
+     -UserPrincipalName $NewUser.UserPrincipalName `
+     -Path $NewUser.OU + ",DC=abstergo,DC=local" `
+     -AccountPassword (ConvertTo-SecureString $NewUser.AccountPassword -AsPlainText -force) `
+     -Enabled $true `
+     -ChangePasswordAtLogon $true
+}
+
+
