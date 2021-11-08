3a6f1d5ccf24fbbc9535ff09d53ace300d91f72b
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-08 22:04:58 +0100

    update

--- setupFallbackServer.ps1
+++ setupFallbackServer.ps1
@@ -7,7 +7,7 @@ Rename-Computer -NewName Fallback -Force
 New-NetIPAddress -IPAddress "192.168.106.6" -PrefixLength "24" -InterfaceIndex (Get-NetAdapter).ifIndex -DefaultGateway "192.168.106.99"
 
 <# changement du dns #>
-Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -ServerAddresses ("127.0.0.1")
+Set-DnsClientServerAddress -InterfaceIndex (Get-NetAdapter).ifIndex -ServerAddresses ("192.168.106.3", "127.0.0.1", "8.8.8.8")
 
 <# changement de nom de la carte réseau #>
 Rename-NetAdapter -Name Ethernet0 -NewName LAN
