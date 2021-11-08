cecca0e850a111a76caa4e59252805047b3fa739
OscarPALISSOT <74594715+OscarPALISSOT@users.noreply.github.com>
2021-11-08 02:43:49 +0100

    ajout dns

--- joinMasterDomain.ps1
+++ joinMasterDomain.ps1
@@ -1,1 +1,7 @@
+
+<# changement du dns #>
+Set-DnsClientServerAddress -InterfaceIndex 9 -ServerAddresses ("192.168.106.3","192.168.106.6","8.8.8.8")
+
+<# ajout au domaine #>
 add-computer –domainname abstergo.local -Credential ABSTERGO\administrateur -restart –force
+
